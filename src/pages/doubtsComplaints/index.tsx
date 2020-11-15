0d253a5a8bacf96d776596c51be130aa0e4790cd
Jonathan Albert Santos Carvalho <jonathanalbertbh89@gmail.com>
2020-11-15 02:18:22 -0300

    criando a pagina de duvida

--- src/pages/doubtsComplaints/index.tsx
+++ src/pages/doubtsComplaints/index.tsx
@@ -1,31 +1,88 @@
 import React, { useState } from 'react';
 import {View, Text} from 'react-native'
-import { TextInput } from 'react-native-gesture-handler';
+import { RectButton, TextInput } from 'react-native-gesture-handler';
 import CheckBox from '@react-native-community/checkbox';
+
+import {Picker} from '@react-native-picker/picker'
+
+import {Feather} from '@expo/vector-icons'
+
+import style from './style'
 
 const DoubtsComplaints: React.FC = () => {
 
     const [doubts, setDoubts] = useState(false);
     const [complaints, setComplaints] = useState(false);
 
+    const data = [
+        {id: 0, title: 'mais serviços' }
+    ]
+
     return(
-        <View>
+        <View style={style.container}>
 
-            <TextInput
-                placeholder= "Eu preciso de ..."
-            />
+            <View style={style.containerSearch}>
+                <TextInput 
+                    style={style.textInputSearch}
+                    placeholder= "Eu preciso de ..."
+                />
+                <Feather name="search" size={22} />
+            </View>
 
-            <View>
+            
+            
+
+            <View
+                style={style.containerCheckBox}
+            >
+                <View style={style.containerCheckBoxAndText}>
                 <CheckBox
                     disabled={false}
                     value={doubts}
-                    boxType='circle'
+                    boxType="circle"
                     onValueChange={(newValue) => setDoubts(newValue)}
                 />
-                
-            </View>
-              
+                <Text style={style.textCheckBox}>Duvida</Text>
+                </View>
 
+                <View style={style.containerCheckBoxAndText}>
+                <CheckBox
+                    disabled={false}
+                    value={complaints}
+                    boxType='circle'
+                    onValueChange={(newValue) => setComplaints(newValue)}
+                />
+                <Text style={style.textCheckBox}>Reclamação</Text>
+                </View>
+
+            </View>
+
+            <TextInput
+                placeholder="Digite seu texto..."
+                style={style.longText}
+            />
+
+            <View
+                style={style.containerButton}
+            >
+                <RectButton 
+                    style={style.buttonCancel}
+                >
+                    <Text style={style.textButton}>Cancelar</Text>
+                </RectButton>
+                <RectButton
+                    style={style.buttonSubmit}
+                >
+                    <Text style={style.textButton}>Enviar</Text>
+                </RectButton>
+            </View>
+
+              
+            <Picker
+                style={style.picker}
+             
+                
+            />
            
 
         </View>
--- src/pages/doubtsComplaints/style.ts
+++ src/pages/doubtsComplaints/style.ts
@@ -2,7 +2,108 @@ import { StyleSheet } from  'react-native'
 
 const styles = StyleSheet.create({
     container:{
+        flex: 1,
+        alignItems: 'center',
 
+    },
+
+    containerSearch:{
+        width: '90%',
+        height: 40,
+
+        marginTop: 35,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: 5,
+
+        borderBottomWidth: 1,
+        borderLeftWidth: 1,
+        borderTopWidth: 1,
+        borderRightWidth: 1,
+
+        borderRadius: 10
+
+    },
+
+    textInputSearch:{
+        width: '90%',
+        height: 60
+    },
+
+    containerCheckBox:{
+        marginTop: 50,
+        width: '90%',
+        height: 80,
+        justifyContent: 'center',
+       
+    },
+
+    containerCheckBoxAndText:{
+        
+        flexDirection: 'row',
+        
+
+    },
+
+    textCheckBox:{
+        fontSize: 18, 
+        justifyContent: 'center'
+    },
+
+    longText:{
+        width: '90%',
+        height: 182,
+        marginTop: 31,
+
+
+        borderBottomWidth: 1,
+        borderLeftWidth: 1,
+        borderTopWidth: 1,
+        borderRightWidth: 1,
+
+        borderRadius: 10
+    },
+
+    containerButton:{
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        width: '90%',
+        height: 60
+    },
+
+    buttonCancel:{
+        width: 90,
+        height: 40,
+        borderRadius: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+
+        backgroundColor: '#C4B9B9',
+
+        marginTop: 12
+    },
+
+    buttonSubmit:{
+        width: 90,
+        height: 40,
+        borderRadius: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+
+        backgroundColor: '#80C342',
+
+        marginTop: 12
+    },
+
+    textButton:{
+        fontSize: 18,
+
+    },
+
+    picker:{
+        width: '90%',
+        height: 50
     }
 })
 
